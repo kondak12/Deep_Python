daff1e171b21dc623a88ca9f33ce96fcc036a7cd
Ivan <ivan.kondakov.98@bk.ru>
2025-07-01 14:50:06 +0300

    Task 2 is completed!

--- hw_4/task 2.py
+++ hw_4/task 2.py
@@ -1,1 +1,25 @@
-{\rtf1}
+import threading
+import random
+import time
+
+
+class MyThread(threading.Thread):
+
+    name: str
+    sleep_time: int
+
+    def __init__(self, name: str, sleep_time: int):
+
+        if sleep_time < 0: raise ValueError("Время сна не может быть отрицательным.")
+
+        self.__name = name
+        self.__sleep_time = sleep_time
+
+    def run(self):
+        print(f"Поток {self.__name} работает")
+        time.sleep(self.__sleep_time)
+
+mt = MyThread("Name!", 3)
+
+for i in range(random.randint(5, 10)):
+    mt.run()
