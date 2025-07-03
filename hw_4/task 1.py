2af9cd6d2c5c7c0700642272998985192602b9ae
Ivan <ivan.kondakov.98@bk.ru>
2025-07-03 16:34:46 +0300

    Update task 1

--- hw_4/task 1.py
+++ hw_4/task 1.py
@@ -1,14 +1,18 @@
-import random
 import threading
 import time
 
 
 def thread_func(th_name):
     print(f"Привет из потока '{th_name}'!")
-    time.sleep(random.randint(1, 3))
+    time.sleep(1)
 
+threads = []
 
-for i in range(random.randint(5, 8)):
+for i in range(1, 7):
     name = f"Поток {i}"
     thread = threading.Thread(target=thread_func(name), name=name)
     thread.start()
+    threads.append(thread)
+
+for th in threads:
+    th.join()
