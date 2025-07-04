1e1ac64847fe5ba795a5b8e33a0afa62ea9f7e31
Ivan <ivan.kondakov.98@bk.ru>
2025-07-04 16:41:10 +0300

    Task 5 is completed!

---
+++ hw_4/task 5.py
@@ -0,0 +1,23 @@
+import threading
+import random
+
+
+shared_list = []
+list_lock = threading.Lock()
+
+def add_random_numbers():
+    with list_lock:
+        [shared_list.append(random.randint(1, 333)) for _ in range(5)]
+
+
+threads = []
+
+for i in range(1, 4):
+    name = f"Поток {i}"
+    thread = threading.Thread(target=add_random_numbers())
+    thread.start()
+    print(shared_list) # "проверка" того, что список обновляется
+    threads.append(thread)
+
+for t in threads:
+    t.join()
