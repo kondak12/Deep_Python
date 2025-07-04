304dfc9c4a4d913b68b68c6334073a50d2fd9caf
Ivan <ivan.kondakov.98@bk.ru>
2025-07-04 16:10:19 +0300

    Update task 4

--- hw_4/task 4.py
+++ hw_4/task 4.py
@@ -8,19 +8,17 @@ def thread_target(name: str, sleep: int):
     print(f"Поток {name} завершен")
 
 
-def thread_logger(thread):
-
-    def wrapper(*args, **kwargs):
-
-        result = thread(*args, **kwargs)
-
-        return result
-
-    return wrapper
-
-
-@thread_logger
 def new_thread(th_name: str, sleep_time: int):
     return threading.Thread(target=thread_target(th_name, sleep_time), name=th_name)
 
-new_thread("123", 2).start()
+threads = [
+    new_thread("123", 2),
+    new_thread("234", 1),
+    new_thread("345", 2)
+]
+
+for t in threads:
+    t.start()
+
+for t in threads:
+    t.join()
