00195387d252c34e99c330ac7942ac1ea22e91c2
Ivan <ivan.kondakov.98@bk.ru>
2025-07-04 16:17:00 +0300

    Update task 3

--- hw_4/task 3.py
+++ hw_4/task 3.py
@@ -3,26 +3,37 @@ import time
 
 
 def thread_counter(func):
+
     thread_count = 0
+
     def wrapper(*args, **kwargs):
         nonlocal thread_count
+
         thread_count += 1
-        result = func(*args, **kwargs)
         print(f"thread count -> {thread_count}")
-        return result
+
+        return func(*args, **kwargs)
+
     return wrapper
+
 
 @thread_counter
 def new_thread(*args, **kwargs):
-    threading.Thread(*args, **kwargs).start()
+    return threading.Thread(*args, **kwargs)
+
 
 def thread_func(thread_count: int, sleep_time=0.0001):
+
+    threads = []
+
     for i in range(thread_count):
         name = f"Поток {i}"
-        new_thread(name=name)
+        thread = new_thread(name=name)
+        thread.start()
+        threads.append(thread)
         time.sleep(sleep_time)
 
+    for t in threads:
+        t.join()
+
 thread_func(100)
-
-
-# я эксперементировал и возможно сделал неправильно
